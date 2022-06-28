2ca3831a524b15e9be66a2da6b69db24f3e6bc33
Timothée Jaussoin <edhelas@movim.eu>
2022-06-28 22:54:13 +0200

    Initiate Laravel project

---
+++ database/migrations/2022_06_28_201257_create_data_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('scenario_id');
+            $table->foreign('scenario_id')->references('id')->on('scenarios')->onDelete('cascade');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->integer('year');
+            $table->integer('capacity');
+            $table->integer('production');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+};

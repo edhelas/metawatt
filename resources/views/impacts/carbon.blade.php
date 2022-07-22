08caec6439881429205fbd533927bda800c33d20
Timothée Jaussoin <edhelas@movim.eu>
2022-07-22 23:58:14 +0200

    Fix label

--- resources/views/impacts/carbon.blade.php
+++ resources/views/impacts/carbon.blade.php
@@ -4,7 +4,8 @@
 
 <h1><i class="fa-graph"></i> Impact <small class="text-muted">Carbone</small></h1>
 
-<p>Émissions totales à l'issue de la transition en {{ $year}}.<br />En gCO2eq/kWh (données IPCC 2014).</p>
+<p>Émissions totales en tonnes de CO2 à l'issue de la transition en {{ $year}}.<br />
+    Sources en gCO2eq/kWh (données IPCC 2014).</p>
 
 @include('parts.graph')
 

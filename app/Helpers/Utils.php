f75aaf4be7531af22b76d378bb9e9ec0d387a349
Timothée Jaussoin <edhelas@movim.eu>
2022-08-19 13:50:51 +0200

    Add description and logos for scenarios groups

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -1,5 +1,7 @@
 <?php
 
+use League\CommonMark\CommonMarkConverter;
+
 function catIcon(string $category): string
 {
     $icons = [
@@ -60,6 +62,68 @@ function groupNameSecond(string $group): string
     return (array_key_exists($group, $groups))
         ? $groups[$group]
         : 'Groupe';
+}
+
+function groupLogo(string $group): string
+{
+    $groups = [
+        'rte' => 'rte.svg',
+        'nw' => 'negawatt.jpg',
+        'ademe' => 'ademe.svg',
+    ];
+
+    return (array_key_exists($group, $groups))
+        ? 'img/' . $groups[$group]
+        : '';
+}
+
+function groupIntroduction(string $group): string
+{
+    $groups = [
+        'rte' => 'En 2019, RTE a lancé une large étude sur l’évolution du système électrique intitulée « Futurs énergétiques 2050 ».
+
+Cette étude implique une démarche inédite en matière de concertation et de transparence impliquant les parties prenantes intéressées à tous les stades de construction des scénarios, jusqu’à la publication des principaux résultats à l’automne 2021 et de leur analyse complète en février 2022.',
+        'belfort' => 'Pierrick Dartois et Marie Suderie ont publié à l’été 2022 leur mémoire de fin de formation du Corps des ingénieurs des Mines.
+
+Leur publication examine les conséquences sur le système électrique d’un mix intégrant du nucléaire et une part importante d’énergies renouvelables selon les orientations formulées par Emmanuel MACRON à Belfort le 10 février 2022.',
+        'nw' => 'L’association negaWatt publie depuis plusieurs année des scénarios de transition énergétique.
+
+Partant du principe que l’énergie la moins polluante est celle qu’on ne consomme/produit pas, négaWatt propose de repenser notre vision de l’énergie en s’appuyant sur une démarche en trois étapes: sobriété, efficacité énergétique et énergies renouvelables.',
+        'ademe' => 'L’ADEME a souhaité soumettre au débat quatre chemins “types” cohérents qui présentent de manière volontairement contrastée des options économiques, techniques et de société pour atteindre la neutralité carbone en 2050.
+
+Imaginés pour la France métropolitaine, ils reposent sur les mêmes données macroéconomiques, démographiques et d’évolution climatique (+2,1°C en 2100). Cependant, ils empruntent des voies distinctes et correspondent à des choix de société différents.',
+    ];
+
+    $converter = new CommonMarkConverter([
+        'html_input' => 'strip',
+        'allow_unsafe_links' => false,
+    ]);
+
+    return (array_key_exists($group, $groups))
+        ? $converter->convert($groups[$group])
+        : '';
+}
+
+function groupSources(string $group): array
+{
+    $groups = [
+        'rte' => [
+            'https://www.rte-france.com/analyses-tendances-et-prospectives/bilan-previsionnel-2050-futurs-energetiques#Lesdonnees' => 'Futurs énergétiques 2050: Les données'
+        ],
+        'belfort' => [
+
+        ],
+        'nw' => [
+            'https://negawatt.org/Scenario-negaWatt-2017-2050' => 'Scénario négaWatt 2017'
+        ],
+        'ademe' => [
+            'https://librairie.ademe.fr/energies-renouvelables-reseaux-et-stockage/5352-prospective-transitions-2050-feuilleton-mix-electrique.html' => 'ADEME - La Librairie: Prospective - Transitions 2050 - Feuilleton Mix électrique'
+        ],
+    ];
+
+    return (array_key_exists($group, $groups))
+        ? $groups[$group]
+        : [];
 }
 
 function carbonIntensity(string $category): float
--- resources/views/scenarios/index.blade.php
+++ resources/views/scenarios/index.blade.php
@@ -9,6 +9,11 @@
 @foreach ($groups as $group => $scenarios)
     <h2>{{ groupName($group) }} <small class="text-muted">{{ groupNameSecond($group) }}</small></h2>
 
+    @if (!empty(groupLogo($group)))
+        <img class="group_logo" src="{{ groupLogo($group) }}"/>
+    @endif
+
+    <p>{!! groupIntroduction($group) !!}</p>
 
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach($scenarios as $scenario)
@@ -29,6 +34,14 @@
             </div>
         @endforeach
     </div>
+
+    @if (!empty(groupSources($group)))
+        <p>Sources :<br />
+            @foreach (groupSources($group) as $url => $title)
+                <i class="fa-solid fa-link"></i> <a href="{{ $url }}">{{ $title }}</a></br>
+            @endforeach
+        </p>
+    @endif
 @endforeach
 
 @endsection

b81b168604d2b92a3c42d6011f671116df3ca6aa
Timothée Jaussoin <edhelas@movim.eu>
2022-08-19 13:54:39 +0200

    Add target blank for scenarios sources

--- resources/views/scenarios/index.blade.php
+++ resources/views/scenarios/index.blade.php
@@ -38,7 +38,7 @@
     @if (!empty(groupSources($group)))
         <p>Sources :<br />
             @foreach (groupSources($group) as $url => $title)
-                <i class="fa-solid fa-link"></i> <a href="{{ $url }}">{{ $title }}</a></br>
+                <i class="fa-solid fa-link"></i> <a href="{{ $url }}" target="_blank">{{ $title }}</a></br>
             @endforeach
         </p>
     @endif

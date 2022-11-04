629ea786d1bf548fa832f9cb052efedd995c53e5
Timothée Jaussoin <edhelas@movim.eu>
2022-11-04 19:48:11 +0100

    Convert the prouction final percentage table to a graph

--- resources/views/impacts/production/show_final.blade.php
+++ resources/views/impacts/production/show_final.blade.php
@@ -12,34 +12,33 @@
 
 <h3>Part de production</h3>
 
-<p>Part en % des différentes sources d'électricité dans la production totale au cours du temps jusqu'en {{ $year }}.</p>
+<p>Part en % des différentes sources d'électricité dans la production totale de aujourd'hui jusqu'en {{ $year }}.</p>
 
-<table class="table text-right">
+<table class="table table-sm table-borderless text-right">
     <thead>
         <tr>
             <th></th>
-            @foreach ($percentages[array_key_first($percentages)] as $category => $value)
-                @if ($category != 'total')
-                <th>
-                    {{ $category }}
-                </th>
-                @endif
-            @endforeach
+            <th></th>
         </tr>
     </thead>
     <tbody>
         @foreach ($percentages as $scenario => $data)
         <tr>
-            <th>
+            <th style="width: 20%;" class="line">
                 <b>{{ $scenario }}</b>
             </th>
-            @foreach ($data as $category => $value)
-                @if ($category != 'total')
-                <td>
-                    {{ percentage($value, $data['total']) }}
-                </td>
-                @endif
-            @endforeach
+            <td style="width: 80%;">
+                @foreach ($data as $category => $value)
+                    @if ($category != 'total')
+                    <span title="{{ catName($category)}} - {{ percentage($value, $data['total']) }}%" class="bar"
+                        style="width: {{ percentage($value * 0.99, $data['total']) }}%; background-color: {{ catColor($category) }};">
+                        @if (percentage($value, $data['total']) > 3)
+                            {{ percentage($value, $data['total']) }}%
+                        @endif
+                    </span>
+                    @endif
+                @endforeach
+            </td>
         </tr>
         @endforeach
     </tbody>

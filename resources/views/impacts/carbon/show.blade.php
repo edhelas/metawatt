c7cb790b1f185ff42cc12676b93aaddb0d3f342b
Timothée Jaussoin <edhelas@movim.eu>
2022-11-05 12:23:11 +0100

    Add info box for missing ADEME fossil energy production

--- resources/views/impacts/carbon/show.blade.php
+++ resources/views/impacts/carbon/show.blade.php
@@ -8,6 +8,8 @@
 <p>Émissions totales en tonnes de CO2 pour la production électrique pour chaque scénario au fil du temps.</p>
 <p>Sources en gCO2eq/kWh (données IPCC 2014 et <a href="https://www.edf.fr/groupe-edf/produire-une-energie-respectueuse-du-climat/lenergie-nucleaire/notre-vision/analyse-cycle-de-vie-du-kwh-nucleaire-dedf">EDF 2022</a> pour le nucléaire).</p>
 
+@include('parts.missing_fossils_ademe')
+
 @include('parts.graph')
 
 

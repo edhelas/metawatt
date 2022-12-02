50eb2156dc46f8b5c5a8a91cf7bb5bedf47a22c5
Timothée Jaussoin <edhelas@movim.eu>
2022-12-03 00:08:22 +0100

    Add goals migration

---
+++ database/migrations/2022_12_02_224111_add_goals_to_scenarios_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('scenarios', function (Blueprint $table) {
+            $table->text('goals')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('scenarios', function (Blueprint $table) {
+            $table->dropColumn('goals');
+        });
+    }
+};
